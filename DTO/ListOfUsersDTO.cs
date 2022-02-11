dddd85347bbdf2cf0e6a42a9e4033b8d89b745d4
mimahlynn <jemimahlynnomillo@gmail.com>
2022-02-11 14:09:29 +0800

    Add files via upload

---
+++ DTO/ListOfUsersDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIDemo.DTO{
+    public class ListOfUsersDTO
+    {
+        public long Page { get; set; }
+        public long PerPage { get; set; }
+        public long Total { get; set; }
+        public long TotalPages { get; set; }
+        public List<Data> Data { get; set; }
+    }
+
+    public class Data
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public Uri Avatar { get; set; }
+    }
+
+}
+
---
+++ DTO/SingleUserDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIDemo.DTO
+{
+    public class SingleUserDTO
+     {
+            public Data Data { get; set; }
+            public Support Support { get; set; }
+     }
+
+    public class DataSingleUser
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Uri Avatar { get; set; }
+    }
+
+    public class Support
+    {
+        public Uri Url { get; set; }
+        public string Text { get; set; }
+    }
+    
+}
